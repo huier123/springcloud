177b03a0518672c438935c802769f2d82ba6cf1c
huier123 <784728884@qq.com>
2021-08-14 16:31:46 +0800

    update

--- springcloud-consumer/src/main/java/com/example/springcloudconsumer/SpringcloudConsumerApplication.java
+++ springcloud-consumer/src/main/java/com/example/springcloudconsumer/SpringcloudConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcloudproducer;
+package com.example.springcloudconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-public class SpringcloudProducerApplication {
+public class SpringcloudConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudProducerApplication.class, args);
+        SpringApplication.run(SpringcloudConsumerApplication.class, args);
     }
 
 }
