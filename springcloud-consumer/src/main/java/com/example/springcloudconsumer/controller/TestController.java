67f213cebdad19a05e71886a559a9c865977068f
huier123 <784728884@qq.com>
2021-08-14 16:42:39 +0800

    update

---
+++ springcloud-consumer/src/main/java/com/example/springcloudconsumer/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.example.springcloudconsumer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testController")
+public class TestController {
+
+
+}
