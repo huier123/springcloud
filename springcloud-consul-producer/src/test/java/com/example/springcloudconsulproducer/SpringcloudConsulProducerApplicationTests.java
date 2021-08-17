8cda151f268baed3dbd9d89f2fde0e922494cd2f
huier123 <784728884@qq.com>
2021-08-17 23:09:42 +0800

    update

---
+++ springcloud-consul-producer/src/test/java/com/example/springcloudconsulproducer/SpringcloudConsulProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconsulproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConsulProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
